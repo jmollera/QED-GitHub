751faf34daa26dcc77ab4a8cf6af0c4ea61c952e
jmollera <77206640+jmollera@users.noreply.github.com>
2025-07-29 15:54:33 +0200

    Actualització de biblioteques,  correccions ortogràfiques, i millora cap. Transformadors de potència.

--- v15.1/src/Python/exemples_resolts/Exemple-13-1.py
+++ v15.1/src/Python/exemples_resolts/Exemple-13-1.py
@@ -9,20 +9,20 @@ def Ia(t):
     Iq = 3.159*np.exp(-t/0.023) + 0.781*np.exp(-t/0.106)
     return 4368*np.hypot(Id, Iq)
 
-def t51(I):
-    """I en ampere. t51 en segons. Cal definir externament el paràmetre T."""
-    return T*(0.014/((I/6000)**0.022 - 1) + 0.0226)
+def t51(I, k):
+    """I en ampere, k adimensional. t51 en segons."""
+    return k*(0.014/((I/6000)**0.022 - 1) + 0.0226)
 
-def integral(tau):
-    """ tau en segons. El valor de la integral és adimensional."""
-    val, _ = integrate.quad(lambda t: 1/t51(Ia(t)), 0, tau)
+def integral(tau, k):
+    """ tau en segons, k paràmetre de t51. El valor de la integral és adimensional."""
+    val, _ = integrate.quad(lambda t: 1/t51(Ia(t), k), a=0, b=tau)
     return val
-
-T = 0.6
 
 def log_format(x, pos):
     decimal_places = int(np.maximum(-np.log10(x), 0))
     return f'{x:.{decimal_places:1d}f}'.replace('.', ',')
+
+T = 0.6
 
 t = np.linspace(0, 3, 500)  # segons
 I = np.linspace(6100, 100_000, 500)  # ampere
@@ -38,22 +38,22 @@ ax.grid(which='major', alpha=1)
 ax.set_xlabel(r'$I$ / A')
 ax.set_ylabel(r'$t$ / s')
 ax.plot(Ia(t), t, 'red', label=r'$I_{\mathrm{a}}(t)$')
-ax.plot(I, t51(I), 'green', label=r'$t_{51}(I)$')
+ax.plot(I, t51(I, k=T), 'green', label=r'$t_{51}(I)$')
 ax.legend(loc='upper right')
 plt.show()
 
-sol = optimize.root(lambda x: integral(x) - 1, x0=0.25)
+sol = optimize.root(lambda x: integral(x, k=T) - 1, x0=0.25)
 print(f'T = {T}')
 print('-'*35)
 print(f"Temps calculat d'actuació: {sol.x[0]:.4f} s")
-print(f'Valor de la integral: {integral(tau=sol.x[0]):.4f}\n')
+print(f'Valor de la integral: {integral(tau=sol.x[0], k=T):.4f}\n')
 print(sol)
 
-T = 0.8
+T=0.8
 
-sol = optimize.root(lambda x: integral(x) - 1, x0=0.25)
+sol = optimize.root(lambda x: integral(x, k=T) - 1, x0=0.25)
 print(f'\nT = {T}')
 print('-'*35)
 print(f"Temps calculat d'actuació: {sol.x[0]:.4f} s")
-print(f'Valor de la integral: {integral(tau=sol.x[0]):.4f}\n')
+print(f'Valor de la integral: {integral(tau=sol.x[0], k=T):.4f}\n')
 print(sol)
